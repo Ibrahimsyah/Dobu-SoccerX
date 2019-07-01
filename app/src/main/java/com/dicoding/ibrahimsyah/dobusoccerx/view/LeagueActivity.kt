149454d1f2f0d17a1936cae5c4aa59937ad429d3
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2019-07-01 08:20:18 +0700

    Added Transparent Status Bar

--- app/src/main/java/com/dicoding/ibrahimsyah/dobusoccerx/view/LeagueActivity.kt
+++ app/src/main/java/com/dicoding/ibrahimsyah/dobusoccerx/view/LeagueActivity.kt
@@ -1,9 +1,11 @@
 package com.dicoding.ibrahimsyah.dobusoccerx.view
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 import com.dicoding.ibrahimsyah.dobusoccerx.R
 import com.dicoding.ibrahimsyah.dobusoccerx.SearchActivity
 import com.dicoding.ibrahimsyah.dobusoccerx.adapter.LeagueViewPagerAdapter
@@ -18,6 +20,12 @@ class LeagueActivity : AppCompatActivity() {
         league_toolbar.title = "English Premiere League"
         setSupportActionBar(league_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
 
         val viewPagerAdapter = LeagueViewPagerAdapter(supportFragmentManager)
         viewPagerAdapter.addFragment(MatchFragment(), "MATCH")
